2f101f9ea19169ac4b5d78f8df0e375eed6901fc
m45-astroX <yu.2876.2346@gmail.com>
2024-06-10 19:21:00 +0900

    Make : mkNTEspec

---
+++ mkNTEspec_rsl/mkNTEspec_rsl_v1.0/README.md
@@ -0,0 +1,13 @@
+# mkNTEspec
+
+\# 2024.06.10 v1 by Yuma Aoki (Kindai Univ.)
+
+
+## 概要
+
+XRISM/Resolveのイベントファイル(uf)からNTEのスペクトルを作成するスクリプト。
+
+
+## 使い方
+
+    $ bash 
---
+++ mkNTEspec_rsl/mkNTEspec_rsl_v1.0/mkNTEspec_rsl_v1.0.bash
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# mkNTEspec_rsl
+
+# 2024.06.10 v1.0 by Yuma Aoki (Kindai Univ.)
+
+
+
+SCRIPT_VERSION=1.0
+
+if [ $# -ne 2 ] ; then
+    echo "Usage : bash mkNTEspec_rsl_v${SCRIPT_VERSION}.bash"
+    echo "    \$1 : BASENAME"
+    echo "    \$2 : evtfile(uf)"
+    exit
+else
+
+    BASENAME=$1
+    infile=$2
+    outfile_evt="${BASENAME}_nte.evt"
+    outfile_pi="${BASENAME}_nte.pi"
+    outfile_lc="${BASENAME}_nte.lc"
+
+    if [ ! -e $infile ] ; then
+        echo "Infile ($infile) does not exist!"
+        echo "abort"
+        exit
+    fi
+    if [ -e $outfile_evt ] ; then
+        rm -f $outfile_evt
+    fi
+    if [ -e $outfile_pi ] ; then
+        rm -f $outfile_pi
+    fi
+    if [ -e $outfile_lc ] ; then
+        rm -f $outfile_lc
+    fi
+
+fi
+
+
+
+### Variables ###
+tmpfile1='tmpfile1_mkNTEspec_rsl.tmp'
+
+
+
+### log files and dirs ###
+d_log=./logfiles_mkNTEspec_rsl
+f_log=$d_log/mkNTEspec_rsl.log
+if [ -e $d_log ] ; then
+    rm -f $d_log/*
+else
+    mkdir $d_log
+fi
+
+
+
+### Extract NTE events ###
+echo "" >> $f_log
+echo "BEGIN Extract NTE events" >> $f_log
+echo "CMD : ahscreen infile=$infile outfile=$outfile gtifile=$infile[GTIEHKNTE] expr=NONE selectfile=NONE label=PIXELALL mergegti=AND" >> $f_log
+echo "" >> $f_log
+
+ahscreen \
+    infile="$infile" \
+    outfile="$tmpfile1" \
+    gtifile="$infile[GTIEHKNTE]" \
+    expr=NONE \
+    selectfile=NONE \
+    label=PIXELALL \
+    mergegti=AND \
+    1>> $f_log 2>> $f_log
+
+# Exclude calpixel
+echo "" >> $f_log
+echo "CMD : fselect $tmpfile1 $outfile_evt PIXEL!=12" >> $f_log
+echo "" >> $f_log
+
+fselect $tmpfile1 $outfile_evt "PIXEL!=12" \
+1>> $f_log 2>> $f_log
+
+echo "" >> $f_log
+echo "END Extract NTE events" >> $f_log
+echo "" >> $f_log
+
+
+
+### Make spectra ###
+echo "BEGIN XSELECT" >> $f_log
+echo "CMD : xselect" >> $f_log
+echo "" >> $f_log
+xselect << EOT
+xsel
+
+! Setup
+set mission XRISM
+
+! Read events
+read events
+.
+$outfile_evt
+
+! Filter events
+filter column "PIXEL=0:11,13:35"
+filter GRADE "0:0"
+
+! Make light curves
+extract curve
+save curve $outfile_lc
+
+! Make PI files
+extract spectrum
+save spectrum $outfile_pi
+
+! End of xselect
+exit
+yes
+EOT
+
+echo "" >> $f_log
+echo "END XSELECT" >> $f_log
+echo "" >> $f_log
+
+
+
+# Move files
+mv \
+    ahscreen.log \
+    xsel_ascii_out.xsl \
+    xsel_display.def \
+    xsel_files.tmp \
+    xsel_fits_curve.xsl \
+    xsel_hist.xsl \
+    xsel_obscat.tmp \
+    xsel_obslist.def \
+    xsel_read_cat.xsl \
+    xsel_session.xsl \
+    xsel_xronos_out.xsl \
+    xselect.log \
+    $d_log/
+
+
+# Zip eventfiles
+gzip $outfile_evt
+
+# Remove files
+rm -f $tmpfile1
+
+
+
+exit
